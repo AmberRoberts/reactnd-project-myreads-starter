5d8ea89f097f7ce46cf689913e383d117df4d54e
Amber Roberts <amberrobertsvt@gmail.com>
2018-09-02 14:18:59 -0400

    Book placeholders display on shelf

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -14,9 +14,10 @@ class BookShelf extends Component {
         <h2 className="bookshelf-title">Currently Reading</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-          <li>
-          <Book />
-          </li>
+          { /* Filter to select books for this specific shelf, then map to create a new array to display here */ }
+          {this.props.books.filter(book => book.shelf === "currentlyReading").map(book => (
+            <li><Book /></li>
+          ))}
           </ol>
         </div>
       </div>
@@ -24,9 +25,9 @@ class BookShelf extends Component {
         <h2 className="bookshelf-title">Want to Read</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-            <li>
-            <Book />
-            </li>
+          {this.props.books.filter(book => book.shelf === "currentlyReading").map(book => (
+            <li><Book /></li>
+          ))}
           </ol>
         </div>
       </div>
@@ -34,9 +35,9 @@ class BookShelf extends Component {
         <h2 className="bookshelf-title">Read</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-            <li>
-            <Book />
-            </li>
+          {this.props.books.filter(book => book.shelf === "currentlyReading").map(book => (
+            <li><Book /></li>
+          ))}
           </ol>
         </div>
       </div>
